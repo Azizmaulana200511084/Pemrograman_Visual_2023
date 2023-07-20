f9c2dffc1a7e6b97fc18b9cc08e4c060bf3138fa
Aziz Maulana <103126797+Azizmaulana200511084@users.noreply.github.com>
2023-07-20 11:02:35 +0700

    Delete QUIZ VISUAL 2023/Mahasiswa_Client_API directory

--- QUIZ VISUAL 2023/Mahasiswa_Client_API/Mahasiswa_Client_API/Matakuliah.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Matakuliah
-    Public Property kodemk As String
-    Public Property namamk As String
-    Public Property sks As String
-    Public Property prodi As String
-End Class
